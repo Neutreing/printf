8106e0f5822582e42b73ecf1d9a43f1d3db74b43
Neutreing <preciousoloye@gmail.com>
2023-11-14 15:41:38 -0800

    correction

--- _printf.c
+++ _printf.c
@@ -1,11 +1,5 @@
 #include "main.h"
 
-/**
- * _printf - Printf function
- * @format: Format string.
- * Return: Always return number of characters printed.
- */
-
 int _printf(const char *format, ...)
 {
 	int print_char = 0;
@@ -13,7 +7,6 @@ int _printf(const char *format, ...)
 
 	if (format == NULL)
 		return (-1);
-
 	va_start(args_list, format);
 
 	while (*format)
@@ -34,14 +27,13 @@ int _printf(const char *format, ...)
 				write(1, format, 1);
 				print_char++;
 			}
-
 			else if (*format == 'c')
 			{
 				char c = va_arg(args_list, int);
+
 				write(1, &c, 1);
 				print_char++;
 			}
-
 			else if (*format == 's')
 			{
 				char *str = va_arg(args_list, char*);
@@ -49,14 +41,12 @@ int _printf(const char *format, ...)
 
 				while (str[str_lent] != '\0')
 					str_lent++;
-
 				write(1, str, str_lent);
 				print_char += str_lent;
 			}
 		}
-
 		format++;
 	}
 	va_end(args_list);
-	return print_char;
+	return (print_char);
 }
