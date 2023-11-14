765b2b436157204996fcb4ba6f8a0341c6caeccb
Neutreing <preciousoloye@gmail.com>
2023-11-14 15:12:29 -0800

    0 task

--- main.h
+++ main.h
@@ -4,6 +4,13 @@
 #include <string.h>
 #include <unistd.h>
 
+int print_c(va_list val_list);
+int print_s(va_list val_list);
+int _strlenc(const char *s);
+int _strlen(char *s);
+int _putchar(char c);
 int _printf(const char *format, ...);
+int print37(void);
+
 
 #endif
---
+++ print_per.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print37 - prints the char %.
+ * Return: 1.
+ */
+int print37(void)
+{
+	_putchar(37);
+	return (1);
+}
