73045178a3e4586cf4a8bc9d3faa7ebcf0e733f0
Neutreing <preciousoloye@gmail.com>
2023-12-14 10:26:48 -0800

    update2

---
+++ handle_int.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * handle_int - handles the 'd' and 'i' conversion specifiers
+ * @args: arguments list
+ *
+ * Return: returns the number of characters printed
+ */
+int handle_int(va_list args)
+{
+	int number = va_arg(args, int);
+	char buffer[12];  // Assuming a maximum of 12 digits for an integer
+	int len = sprintf(buffer, "%d", number);
+
+	write(1, buffer, len);
+
+	return len;
+}
