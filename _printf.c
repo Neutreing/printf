8b332e1a6d7a34a0d9208a278fc386ed5a53095a
Neutreing <preciousoloye@gmail.com>
2023-12-14 10:03:34 -0800

    update

--- _printf.c
+++ _printf.c
@@ -1,58 +1,51 @@
 #include "main.h"
 
 /**
- * _printf - Printf function
- * @format: Format string.
- * Return: Always return number of characters printed.
+ * _printf - prints data to stdout
+ * @format: string to be printed
+ *
+ * Return: returns no of characters to be printed
  */
-
 int _printf(const char *format, ...)
 {
-	int print_char = 0;
-	va_list args_list;
+	size_t i;
+	int no_of_chars = 0;
+	va_list args;
 
 	if (format == NULL)
 		return (-1);
-	va_start(args_list, format);
-
-	while (*format)
+	va_start(args, format);
+	for (i = 0; i < strlen(format); i++)
 	{
-		if (*format != '%')
+		if (format[i] != '%')
 		{
-			write(1, format, 1);
-			print_char++;
-		}
-		else
+			write(1, &format[i], 1);
+			no_of_chars++;
+		} else
 		{
-			format++;
-			if (*format == '\0')
+			i++;
+			if (format[i] == '\0')
 				break;
-
-			if (*format == '%')
+			if (format[i] == 'c')
 			{
-				write(1, format, 1);
-				print_char++;
-			}
-			else if (*format == 'c')
-			{
-				char c = va_arg(args_list, int);
+				char letter = va_arg(args, int);
 
-				write(1, &c, 1);
-				print_char++;
-			}
-			else if (*format == 's')
+				write(1, &letter, 1);
+				no_of_chars++;
+			} else if (format[i] == 's')
 			{
-				char *str = va_arg(args_list, char*);
-				int str_lent = 0;
+				char *str = va_arg(args, char *);
+				int len = strlen(str);
 
-				while (str[str_lent] != '\0')
-					str_lent++;
-				write(1, str, str_lent);
-				print_char += str_lent;
+				write(1, str, len);
+				no_of_chars += len;
+			} else if (format[i] == '%')
+			{
+				write(1, "%", 1);
+				no_of_chars++;
 			}
 		}
-		format++;
 	}
-	va_end(args_list);
-	return (print_char);
+	va_end(args);
+	return (no_of_chars);
 }
